dcededd06a47de8be02d19e69f67f990c9322e1d
Avi <95044844+Avioti@users.noreply.github.com>
2025-10-03 13:35:43 -0400

    Added in Book donations

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -6,26 +6,26 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Book[] bookInventory= {
-                new Book(1, "978-0132350838", "The Phoenix Project", "", false),
-                new Book(2, "978-0132350884", "Clean Code", "", false),
-                new Book(3, "978-0201633610", "Design Patterns", "", false),
-                new Book(4, "978-0136291558", "Object Oriented Software Engineering", "", false),
-                new Book(5, "978-0137081073", "The Clean Coder", "", false),
-                new Book(6, "978-0134685991", "Effective Java", "", false),
-                new Book(7, "978-0135957059", "The Pragmatic Programmer", "", false),
-                new Book(8, "978-0321125215", "Domain-Driven Design", "", false),
-                new Book(9, "978-0201616224", "The Pragmatic Programmer (1st Ed)", "", false),
-                new Book(10, "978-1449373320", "Designing Data-Intensive Applications", "", false),
-                new Book(11, "978-0596517748", "JavaScript: The Good Parts", "", false),
-                new Book(12, "978-0262033848", "Introduction to Algorithms", "", false),
-                new Book(13, "978-0134494166", "Clean Architecture", "", false),
-                new Book(14, "978-1617294136", "Grokking Algorithms", "", false),
-                new Book(15, "978-0321573513", "Algorithms", "", false),
-                new Book(16, "978-1491950357", "Building Microservices", "", false),
-                new Book(17, "978-0135974445", "Refactoring", "", false),
-                new Book(18, "978-0672337383", "Code Complete", "", false),
-                new Book(19, "978-0596007126", "Head First Design Patterns", "", false),
-                new Book(20, "978-1492040347", "Kubernetes: Up and Running", "", false)
+                new Book(1, "978-0134685991", "Effective Java", "", false),
+                new Book(2, "978-0596009205", "Head First Java", "", false),
+                new Book(3, "978-0321356683", "Effective Java (2nd Edition)", "", false),
+                new Book(4, "978-1617293290", "Java 8 in Action", "", false),
+                new Book(5, "978-1491981955", "Mastering Ethereum", "", false),
+                new Book(6, "978-1484252772", "Beginning Ethereum Smart Contracts Programming", "", false),
+                new Book(7, "978-1484250389", "Mastering Blockchain Programming with Solidity", "", false),
+                new Book(8, "978-1119555018", "Blockchain Development with Ethereum", "", false),
+                new Book(9, "978-1491971949", "Mastering Bitcoin and Ethereum", "", false),
+                new Book(10, "978-0134190440", "The Java Language Specification", "", false),
+                new Book(11, "978-0596516178", "Java Concurrency in Practice", "", false),
+                new Book(12, "978-1449374648", "Ethereum Development with Go", "", false),
+                new Book(13, "978-0262033848", "Introduction to Algorithms", "", false),
+                new Book(14, "978-0393347944", "The Selfish Gene", "", false),
+                new Book(15, "978-0393349948", "The Greatest Show on Earth", "", false),
+                new Book(16, "978-0226901169", "The Evolution of Cooperation", "", false),
+                new Book(17, "978-0815344322", "Molecular Biology of the Cell", "", false),
+                new Book(18, "978-0393355482", "Why Evolution Is True", "", false),
+                new Book(19, "978-0393929270", "The Origin of Species", "", false),
+                new Book(20, "978-0879694357", "Evolution (Douglas Futuyma)", "", false)
         };
 
         while(true){
@@ -72,6 +72,9 @@ public class App {
                             }
                         }
                     }
+                    break;
+                case 3:
+                    bookInventory = donateBook(scanner, bookInventory);
                     break;
 
                 default:
@@ -126,11 +129,34 @@ public class App {
             }
         }
     }
+    public static Book[] donateBook(Scanner input, Book[] inventory){
+        System.out.println("Enter the ISBN of the book you want to donate: ");
+        String isbn = input.nextLine();
+        System.out.println("Enter the title of the book you want to donate: ");
+        String title = input.nextLine();
+
+        int newId = inventory.length + 1;
+        Book newBook = new Book(newId, isbn, title, "", false);
+
+        Book[] newInventory = new Book[inventory.length + 1];
+        for (int i = 0; i < inventory.length + 1; i++) {
+            if (i == inventory.length) {
+                newInventory[i] = newBook;
+            } else {
+                newInventory[i] = inventory[i];
+            }
+        }
+
+        System.out.println("Thank you for donating " + title + "!");
+
+        return newInventory;
+    }
 
     public static void displayHomeScreen(){
         System.out.println("1 - Show Available Books");
         System.out.println("2 - Show Checked Out Books");
-        System.out.println("3 - Exit - Close out the Application");
+        System.out.println("3 - Donate a Book of your choice");
+        System.out.println("4 - Exit - Close out the Application");
 
         System.out.printf("%nEnter Option: ");
     }
